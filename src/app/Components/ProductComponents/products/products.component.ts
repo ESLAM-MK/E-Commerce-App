628ced9f384dbac9d614388b2b10d4a05aeeaffd
EsraaMohamed90 <sirymo615@gmail.com>
2024-09-28 15:01:46 +0200

    conecte back with front

--- src/app/Components/ProductComponents/products/products.component.ts
+++ src/app/Components/ProductComponents/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { ProductserviceService } from '../../../Services/productservice.service';
@@ -8,38 +8,54 @@ import { CommonModule, NgClass, NgStyle } from '@angular/common';
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [FormsModule,CommonModule,RouterLink,NgClass,NgStyle],
+  imports: [FormsModule, CommonModule, RouterLink, NgClass, NgStyle],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
-  productListData:any[]=[]
-  searchName:string=' '
-  filteredProductListData:any[]=[]
-  constructor(private prodServ:ProductserviceService,private router:Router,private toastr: ToastrService){}
-    ngOnInit(): void {
-      this.productListData =this.prodServ.getAllProducts()
-      this.filteredProductListData=this.prodServ.getAllProducts()
-      console.log(this.filteredProductListData)
-    }
-    addToCart(product:any){
-      this.prodServ.addToCart(product)
-    }
-    removeFromCart(productId:number){
-      this.prodServ.removeFromCart(productId)
-    }
-    onKeyUp(name:string){
-      this.searchName=name
-      console.log(name)
-      this.filteredProductListData =this.productListData.filter((product)=>{
-        return  product.title.includes(this.searchName)}
-      )
-      }
-      searchByName($event: Event){
-        const input =$event.target as HTMLInputElement
-        this.filteredProductListData =this.productListData.filter((product)=>{
-         return  product.title.toLowerCase().includes(input.value.toLowerCase())
-        })
+export class ProductsComponent implements OnInit {
+  productListData: any[] = [];
+  searchName: string = '';
+  filteredProductListData: any[] = [];
 
-      }
+  constructor(private prodServ: ProductserviceService, private router: Router, private toastr: ToastrService) {}
+
+  ngOnInit(): void {
+    // Subscribe to the observable to get the product list
+    this.prodServ.getAllProducts().subscribe(data => {
+      this.productListData = data;
+      this.filteredProductListData = data; // Initialize filtered list
+      console.log(this.filteredProductListData);
+    });
+  }
+
+  addToCart(product: any) {
+    this.prodServ.addToCart(product.id).subscribe(response => {
+      this.toastr.success('Product added to cart', 'Success');
+    }, error => {
+      this.toastr.error('Failed to add product to cart', 'Error');
+    });
+  }
+
+  removeFromCart(productId: number) {
+    this.prodServ.removeFromCart(productId).subscribe(response => {
+      this.toastr.success('Product removed from cart', 'Success');
+    }, error => {
+      this.toastr.error('Failed to remove product from cart', 'Error');
+    });
+  }
+
+  onKeyUp(name: string) {
+    this.searchName = name;
+    console.log(name);
+    this.filteredProductListData = this.productListData.filter((product) => {
+      return product.title.includes(this.searchName);
+    });
+  }
+
+  searchByName($event: Event) {
+    const input = $event.target as HTMLInputElement;
+    this.filteredProductListData = this.productListData.filter((product) => {
+      return product.title.toLowerCase().includes(input.value.toLowerCase());
+    });
+  }
 }
