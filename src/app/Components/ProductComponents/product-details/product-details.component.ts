6c0dcf8743ebe4c939fc92c59808f907b60bb643
EsraaMohamed90 <sirymo615@gmail.com>
2024-09-28 16:08:14 +0200

    fix the button and the product ditalis

--- src/app/Components/ProductComponents/product-details/product-details.component.ts
+++ src/app/Components/ProductComponents/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import {  Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProductserviceService } from '../../../Services/productservice.service';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CommonModule, NgClass } from '@angular/common';
@@ -7,24 +7,37 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [FormsModule,CommonModule,RouterLink,NgClass],
+  imports: [FormsModule, CommonModule, RouterLink, NgClass],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrls: ['./product-details.component.css'] 
 })
 export class ProductDetailsComponent {
-  prodDetails:any={}
-  id ! :number
-  constructor(private prodserv:ProductserviceService, private route:ActivatedRoute){}
+  prodDetails: any = {};
+  id!: number;
+
+  constructor(private prodServ: ProductserviceService, private route: ActivatedRoute) {}
+
   ngOnInit() {
-    this.id =Number(this.route.snapshot.paramMap.get('id'))
-    this.prodDetails=this.prodserv.getProductDetails(this.id)
-    console.log(this.prodDetails)
-  }
-  addToCart(product:any){
-    this.prodserv.addToCart(product)
-  }
-  removeFromCart(productId:number){
-    this.prodserv.removeFromCart(productId)
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.prodServ.getProductDetails(this.id).subscribe(
+      (data) => {
+        this.prodDetails = data;
+      },
+      (error) => {
+        console.error('Error fetching product details:', error);
+      }
+    );
   }
 
+  addToCart(product: any) {
+    this.prodServ.addToCart(product.id).subscribe(() => {
+      console.log('Product added to cart');
+    });
+  }
+
+  removeFromCart(productId: number) {
+    this.prodServ.removeFromCart(productId).subscribe(() => {
+      console.log('Product removed from cart');
+    });
+  }
 }
