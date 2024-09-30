de570f1704eb79e8f17618af6c40ebf55b9b3eee
HazemAhmedSaad <zozahmed939@gmail.com>
2024-09-30 03:58:24 +0300

    update navbar page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductsComponent } from './Components/ProductComponents/products/products.component';
-import { ProductDetailsComponent } from './Components/ProductComponents/product-details/product-details.component';
-import { CartComponent } from './Components/ProductComponents/cart/cart.component';
-import { NotFoundComponent } from './Components/defaultComponents/not-found/not-found.component';
 import { NavBarComponent } from './Components/defaultComponents/nav-bar/nav-bar.component';
 import { FooterComponent } from './Components/defaultComponents/footer/footer.component';
-import { ContactUsComponent } from './Components/defaultComponents/contact-us/contact-us.component';
-import { AboutComponent } from './Components/defaultComponents/about/about.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ProductsComponent,ProductDetailsComponent,CartComponent,NotFoundComponent
-,NavBarComponent,FooterComponent,ContactUsComponent,AboutComponent
-  ],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
